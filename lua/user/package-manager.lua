700382d445c1b82a14d12c5e5e5be0d6dbc0bc26
Dan Dowd <danieldowd89@gmail.com>
2023-03-12 22:59:11 -0600

    adds neoconf

--- lua/user/package-manager.lua
+++ lua/user/package-manager.lua
@@ -42,15 +42,16 @@ require("lazy").setup({
 			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
 			{ "hrsh7th/cmp-buffer" }, -- Optional
 			{ "hrsh7th/cmp-path" }, -- Optional
-			{ "saadparwaiz1/cmp_luasnip" }, -- Optional
 			{ "hrsh7th/cmp-nvim-lua" }, -- Optional
 
 			-- Snippets
 			{ "L3MON4D3/LuaSnip" }, -- Required
-			{ "rafamadriz/friendly-snippets" }, -- Optional
 		},
 	},
 })
+
+-- This must come before lsp config
+require("neoconf").setup({})
 
 require("mason").setup()
 require("mason-lspconfig").setup()
