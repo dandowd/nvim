a566afcedb18beec13a2a865962cc15607b3326f
Dan Dowd <danieldowd89@gmail.com>
2023-03-28 19:25:28 -0600

    adds dap ui back

--- lua/user/dap-config.lua
+++ lua/user/dap-config.lua
@@ -28,19 +28,4 @@ dap.configurations.cs = {
 	},
 }
 
-local dapui = require("dapui")
-
-dapui.setup({
-	layouts = {
-		{
-			elements = {
-				{ id = "scopes", size = 0.25 },
-				{ id = "stacks", size = 0.25 },
-				{ id = "watches", size = 0.25 },
-				{ id = "breakpoints", size = 0.25 },
-			},
-			position = "left",
-			size = 40,
-		},
-	},
-})
+require("dapui").setup()
