3796f0b3bd06f89ff4c32a6b90752783c3b6433b
Dan Dowd <danieldowd89@gmail.com>
2023-04-07 11:19:44 -0600

    removes duplicate augroup

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -12,9 +12,6 @@ local lsp_formatting = function(bufnr)
 	})
 end
 
--- if you want to set up formatting on save, you can use this as a callback
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
 -- add to your shared on_attach callback
 local on_attach = function(client, bufnr)
 	if client.supports_method("textDocument/formatting") then
