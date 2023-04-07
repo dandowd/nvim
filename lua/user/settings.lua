401fa73c727908ccf48684dbb0f9dd1c07bcde22
Dan Dowd <danieldowd89@gmail.com>
2023-04-07 11:21:46 -0600

    removes indent from options

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,13 +1,7 @@
-vim.cmd([[ colorscheme rose-pine ]])
-
-vim.cmd([[ set splitright ]])
-
 vim.o.signcolumn = "yes"
 
 vim.o.number = true
 vim.o.relativenumber = true
-
-vim.o.smartindent = true
 
 vim.o.swapfile = false
 
@@ -18,4 +12,6 @@ vim.o.termguicolors = true
 
 vim.o.updatetime = 50
 
-vim.o.smartindent = true
+vim.cmd([[ colorscheme rose-pine ]])
+
+vim.cmd([[ set splitright ]])
