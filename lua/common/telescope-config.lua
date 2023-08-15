7c66ba3bb72da3534222afcad21053b2295c6956
Dan Dowd <danieldowd89@gmail.com>
2023-08-15 13:55:05 -0600

    adds hidden files to find files

--- lua/common/telescope-config.lua
+++ lua/common/telescope-config.lua
@@ -1,7 +1,7 @@
 local builtin = require("telescope.builtin")
 local extensions = require("telescope").extensions
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>ff", function() builtin.find_files({ hidden = true }) end, {})
 vim.keymap.set("n", "<leader>fp", extensions.projects.projects, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
